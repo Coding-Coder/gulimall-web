e2acc6b86f4d51c2a31a027a54dd7d7a6467db16
lxy <aethon47@163.com>
2021-06-30 15:28:48 +0800

    feat:完善商品的属性分组

--- renren-fast-vue/src/views/modules/common/category-cascader.vue
+++ renren-fast-vue/src/views/modules/common/category-cascader.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- 
+  <!-- 
 使用说明：
 1）、引入category-cascader.vue
 2）、语法：<category-cascader :catelogPath.sync="catelogPath"></category-cascader>
@@ -8,35 +8,22 @@
           由于有sync修饰符，所以cascader路径变化以后自动会修改父的catelogPath，这是结合子组件this.$emit("update:catelogPath",v);做的
       -->
   <div>
-    <el-cascader
-      filterable
-      clearable 
-      placeholder="试试搜索：手机"
-      v-model="paths"
-      :options="categorys"
-      :props="setting"
-    ></el-cascader>
+    <el-cascader filterable clearable placeholder="试试搜索：手机" v-model="paths" :options="categorys" :props="setting"></el-cascader>
   </div>
 </template>
 
 <script>
-//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
-//例如：import 《组件名称》 from '《组件路径》';
-
 export default {
-  //import引入的组件需要注入到对象中才能使用
-  components: {},
   //接受父组件传来的值
   props: {
     catelogPath: {
       type: Array,
-      default(){
+      default() {
         return [];
       }
     }
   },
   data() {
-    //这里存放数据
     return {
       setting: {
         value: "catId",
@@ -47,17 +34,19 @@ export default {
       paths: this.catelogPath
     };
   },
-  watch:{
-    catelogPath(v){
+  created() {
+    this.getCategorys();
+  },
+  watch: {
+    catelogPath(v) {
       this.paths = this.catelogPath;
     },
-    paths(v){
-      this.$emit("update:catelogPath",v);
+    paths(v) {
+      this.$emit("update:catelogPath", v);
       //还可以使用pubsub-js进行传值
-      this.PubSub.publish("catPath",v);
+      this.PubSub.publish("catPath", v);
     }
   },
-  //方法集合
   methods: {
     getCategorys() {
       this.$http({
@@ -67,12 +56,6 @@ export default {
         this.categorys = data.data;
       });
     }
-  },
-  //生命周期 - 创建完成（可以访问当前this实例）
-  created() {
-    this.getCategorys();
   }
 };
 </script>
-<style scoped>
-</style>
