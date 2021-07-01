02a437156bd7055ae17a99183ca800650254fbca
lxy <aethon47@163.com>
2021-07-01 10:27:29 +0800

    style:格式化文档

--- renren-fast-vue/src/views/common/home.vue
+++ renren-fast-vue/src/views/common/home.vue
@@ -27,13 +27,13 @@
 </template>
 
 <script>
-  export default {
-  }
+export default {
+}
 </script>
 
 <style>
-  .mod-home {
-    line-height: 1.5;
-  }
+.mod-home {
+  line-height: 1.5;
+}
 </style>
 
